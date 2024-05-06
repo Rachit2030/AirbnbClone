d11a08fbb7a3e184b6fc7e078e58a02833779c50
Rachit2030 <goyalrachit2030@gmail.com>
2024-05-06 14:08:09 +0530

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Rachit on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
