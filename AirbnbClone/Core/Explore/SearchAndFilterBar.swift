3fef132b0ea826ca38a49e48feeb1cf5965e57c3
Rachit2030 <goyalrachit2030@gmail.com>
2024-05-06 14:52:54 +0530

    Added Search and Filter Bar UI

---
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,45 @@
+//
+//  SearchAndFilterBar.swift
+//  AirbnbClone
+//
+//  Created by Rachit on 06/05/24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment:.leading,spacing:2){
+                Text("Where To")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any Week - Add Guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            
+            Button{
+                
+            }label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    
+            }.foregroundStyle(.black)
+        }.padding(.horizontal)
+            .padding(.vertical,10)
+            .overlay{
+                Capsule()
+                    .stroke(lineWidth: 0.5)
+                    .foregroundStyle(Color(.systemGray4))
+                    .shadow(color:.black.opacity(0.4),radius: 2)
+            }.padding()
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
